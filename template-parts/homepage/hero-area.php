602e901425846e852c6db9b33ef4ab8902bdfa5f
Ardijan Curi <45972576+ardijancuri@users.noreply.github.com>
2026-02-17 21:46:59 +0000

    fix: hero thumbs slightly wider (w-24)

--- template-parts/homepage/hero-area.php
+++ template-parts/homepage/hero-area.php
@@ -141,7 +141,7 @@ if ( is_wp_error( $categories ) ) {
 					<div class="swiper flavor-hero-thumbs mt-2 hidden md:block" id="heroThumbs">
 						<div class="swiper-wrapper">
 							<?php foreach ( $slides as $i => $slide ) : ?>
-								<div class="swiper-slide !w-20 cursor-pointer">
+								<div class="swiper-slide !w-24 cursor-pointer">
 									<img src="<?php echo esc_url( $slide['img'] ); ?>"
 										 alt="" class="w-full h-14 object-cover rounded opacity-50 transition-all"
 										 loading="lazy">
